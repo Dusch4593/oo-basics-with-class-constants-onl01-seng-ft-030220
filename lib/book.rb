ce5913f7be7475d6bf7ad1f13ed804171c4ad7a7
Brandon Dusch <brandondusch@gmail.com>
2020-03-11 20:02:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,9 @@
 class Book
+<<<<<<< HEAD
   attr_accessor :author, :page_count
+=======
+  attr_accessor :author, :page_count,
+>>>>>>> b83583fbae664dcb3075d15e9aeee35e9dfb7057
   attr_reader :title, :genre 
   
   GENRES = []
